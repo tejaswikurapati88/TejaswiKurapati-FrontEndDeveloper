22ea379512d1ac34338974decf598e75fa8f8112
TejaswiKurapati <tejaswikurapati88@gmail.com>
2024-10-16 15:38:10 +0530

    trade

--- src/components/LandingPage/index.js
+++ src/components/LandingPage/index.js
@@ -1,17 +1,27 @@
 import './index.css'
 import NavBar from '../NavBar'
+import Trade from '../Trade'
 
 const LandingPage=()=>{
     return (
-        <div className='bg-container'>
-            <NavBar />
-            <h1 className='main-heading'>When Innovation <br/>
-              Meets <span className='heading-span'>Investment</span>
-            </h1>
-            <p className='para'>Empowering Trading Through Advanced Technology </p>
-            <button className='openapp-button' type='button' >Open app</button>
-            <h1 className='features-heading'>Our Features</h1>
-        </div>
+        <>
+            <div className='bg-container'>
+                <NavBar />
+                <h1 className='main-heading'>When Innovation <br/>
+                Meets <span className='heading-span'>Investment</span>
+                </h1>
+                <p className='para'>Empowering Trading Through Advanced Technology </p>
+                <button className='openapp-button' type='button' >Open app</button>
+                
+
+            </div>
+            <div className='features-container'>
+                    <h1 className='features-heading'>Our Features</h1>
+                    <div className='trade-bg-cont'>
+                        <Trade />
+                    </div>
+                </div>
+        </>
     )
 }
 
---
+++ src/components/Trade/index.js
@@ -0,0 +1,27 @@
+import './index.css'
+
+const Trade=()=>(
+    <div >
+        <div  className="Trade-con">
+            <img className='imgs' src='https://res.cloudinary.com/dkvptcm7q/image/upload/v1729070509/Overlay_fx2z77.png' alt='trade'/>
+            <img  className='imgs' src='https://res.cloudinary.com/dkvptcm7q/image/upload/v1729070951/Container_2_dya3jy.png' alt='trade'/>
+            <img className='imgs' src='https://res.cloudinary.com/dkvptcm7q/image/upload/v1729071015/Overlay_1_sgkhyc.png' alt='trade'/>
+            </div>
+            <div  className="Trade-con">
+            <img className='imgs' src='https://res.cloudinary.com/dkvptcm7q/image/upload/v1729071143/Overlay_2_qvooy9.png' alt='trade'/>
+            <img className='imgs' src='https://res.cloudinary.com/dkvptcm7q/image/upload/v1729072317/Overlay_3_mef9hp.png' alt='trade' />
+        
+        </div>
+        <div className='sm-trade-con'>
+            <img className='sm-imgs' src='https://res.cloudinary.com/dkvptcm7q/image/upload/v1729072676/Overlay_4_ttnqly.png' alt='sm-tade'/>
+            <img className='sm-imgs' src='https://res.cloudinary.com/dkvptcm7q/image/upload/v1729072788/Overlay_5_vtiiqo.png' alt='sm-trade'/> 
+            <img className='sm-imgs' src='https://res.cloudinary.com/dkvptcm7q/image/upload/v1729073076/Overlay_6_ninv2m.png' alt='sm-trade'/> 
+            <img className='sm-imgs' src='https://res.cloudinary.com/dkvptcm7q/image/upload/v1729073171/Overlay_7_l0mqoe.png' alt='sm-trade'/> 
+            <img className='sm-imgs' src='https://res.cloudinary.com/dkvptcm7q/image/upload/v1729073204/Overlay_8_cjdypf.png' alt='sm-trade'/> 
+        
+        </div>
+    </div>
+    
+)
+
+export default Trade
